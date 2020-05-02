2c7d953f8abfa5dc94cd0c943946dce6f7b79e71
SoonMinKwun <jaeyoung0509@naver.com>
2020-05-02 13:57:29 +0900

    first

---
+++ lob/lob/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class lob(models.Model):
+    id=models.models.CharField(_(""), max_length=50)
+        
+# Create your models here.
---
+++ lob/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\t0102\lob\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
